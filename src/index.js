19ef8b8b1579de9631ed9f418d1cf4ec6ce66443
github username <github email address>
2018-07-02 14:06:08 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
   // your solution here
   // grab DOM elements
   const listDiv = document.getElementById("app-content");
-
-  const app = new TaskLister();
+  const submitBtn = document.getElementById("submitButton");
+  const listText = document.getElementById("new-list-title");
+  submitBtn.addEventListener('click', event => {
+    listName = listText.value;
+    const newList = new List(listName);
+    debugger
+  })
+  // const app = new TaskLister();
 });
--- src/list.js
+++ src/list.js
@@ -1,3 +1,20 @@
 class List {
-  // your code here
+    constructor(listName) {
+      this.listName = listName;
+    }
+    buildList(){
+      const newDIV = document.createElement('DIV');
+      const listID = document.getElementById('lists');
+      const h2 = document.createElement('H2');
+      h2.innerHTML = this.listName;
+      const btn = document.createElement('BUTTON');
+      btn.dataset.title = this.listName;
+      btn.innerHTML = "X";
+      btn.setAttribute('class','delete-list');
+      h2.appendChild(btn);
+      newDIV.appendChild(h2);
+      listID.appendChild(newDIV);
+    }
+    render() {
+    }
 }
